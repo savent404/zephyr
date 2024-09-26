75021d8eac83d6c67401da6ac33ba227ba51791f
Savent Gate <savent_gate@outlook.com>
2024-09-26 22:34:47 +0800

    board: fixup jlink options
    Fixup options of jlink debugger.
    
    Currently we can use `ninja -C build debugserver`
    for debugging.
    
    Signed-off-by: Savent Gate <savent_gate@outlook.com>

--- boards/systech/eval_fmql20/board.cmake
+++ boards/systech/eval_fmql20/board.cmake
@@ -3,6 +3,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-board_runner_args(jlink "--device=cortex-a7" "--speed=4000")
+board_runner_args(jlink "--iface=JTAG"
+	"--device=cortex-a7"
+	"--speed=4000"
+	"--tool-opt=-JTAGConf -1,-1"
+	"--no-reset-after-load")
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
