41770b95d8c741da6a57239454c62eb0a98d88a6
Sylvio Alves <sylvio.alves@espressif.com>
2024-09-26 18:06:45 -0300

    soc: esp32s3: fix initialization code
    ESP32-S3 initialization code should apply the errata
    after cache initialization. This fixes it making sure
    data and cache instruction are properly
    handled and let following calls to work as needed.
    
    This also update hal_espressif to force gcc to treat
    register bitfield structs declared as volatile to
    ensure writes on 32 bit peripheral registers.
    
    Fixes #71397
    Fixes #76325
    
    Signed-off-by: Sylvio Alves <sylvio.alves@espressif.com>
    Signed-off-by: Savent Gate <savent_gate@outlook.com>

--- soc/espressif/esp32s3/soc.c
+++ soc/espressif/esp32s3/soc.c
@@ -145,13 +145,28 @@ void IRAM_ATTR __esp_platform_start(void)
 	 */
 	esp_config_data_cache_mode();
 
+	/* Apply SoC patches */
+	esp_errata();
+
+	/* ESP-IDF/MCUboot 2nd stage bootloader enables RTC WDT to check on startup sequence
+	 * related issues in application. Hence disable that as we are about to start
+	 * Zephyr environment.
+	 */
+	wdt_hal_context_t rtc_wdt_ctx = {.inst = WDT_RWDT, .rwdt_dev = &RTCCNTL};
+
+	wdt_hal_write_protect_disable(&rtc_wdt_ctx);
+	wdt_hal_disable(&rtc_wdt_ctx);
+	wdt_hal_write_protect_enable(&rtc_wdt_ctx);
+
+	esp_reset_reason_init();
+
+	esp_timer_early_init();
+
 	esp_mspi_pin_init();
 
 	esp_flash_app_init();
 
 	mspi_timing_flash_tuning();
-
-	esp_mmu_map_init();
 
 	esp_mmu_map_init();
 
