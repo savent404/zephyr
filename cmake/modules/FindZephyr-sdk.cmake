3f8416922fa77d78307ad9a87eff3e35b37fc8d0
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2024-10-22 09:49:34 +0200

    cmake: support range for find_package(Zephyr-sdk)
    Fixes: #80200
    
    CMake `find_package(<package> <version>)` support the use of ranges,
    like `1.0.0...4.0.0`.
    
    Update the FindZephyr-sdk.cmake module to support this.
    This allows looking up the Zephyr SDK with an upper boundry, for example
    `find_package(Zephyr-sdk 0.16...<0.17)`.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
    Signed-off-by: Savent Gate <savent_gate@outlook.com>

--- cmake/modules/FindZephyr-sdk.cmake
+++ cmake/modules/FindZephyr-sdk.cmake
@@ -77,7 +77,7 @@ if(("zephyr" STREQUAL ${ZEPHYR_TOOLCHAIN_VARIANT}) OR
     # To support Zephyr SDK tools (DTC, and other tools) with 3rd party toolchains
     # then we keep track of current toolchain variant.
     set(ZEPHYR_CURRENT_TOOLCHAIN_VARIANT ${ZEPHYR_TOOLCHAIN_VARIANT})
-    find_package(Zephyr-sdk ${Zephyr-sdk_FIND_VERSION}
+    find_package(Zephyr-sdk ${Zephyr-sdk_FIND_VERSION_COMPLETE}
                  REQUIRED QUIET CONFIG HINTS ${ZEPHYR_SDK_INSTALL_DIR}
     )
     if(DEFINED ZEPHYR_CURRENT_TOOLCHAIN_VARIANT)
@@ -108,6 +108,14 @@ if(("zephyr" STREQUAL ${ZEPHYR_TOOLCHAIN_VARIANT}) OR
 
     list(REMOVE_DUPLICATES Zephyr-sdk_CONSIDERED_VERSIONS)
     list(SORT Zephyr-sdk_CONSIDERED_VERSIONS COMPARE NATURAL ORDER DESCENDING)
+    if("${Zephyr-sdk_FIND_VERSION_RANGE_MAX}" STREQUAL "INCLUDE")
+      set(upper_bound _EQUAL)
+    endif()
+
+    if(NOT DEFINED Zephyr-sdk_FIND_VERSION_RANGE)
+      # Range not given, max out to ensure max version is not in effect.
+      set(Zephyr-sdk_FIND_VERSION_MAX 99999999)
+    endif()
 
     if(LIST IN_LIST Zephyr-sdk_FIND_COMPONENTS)
       set(Zephyr-sdk)
