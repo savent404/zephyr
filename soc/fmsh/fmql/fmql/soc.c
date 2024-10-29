0cc698d432145af7109de335c592198c5b9291ce
Savent Gate <savent_gate@outlook.com>
2024-10-29 09:28:06 +0800

    fmql: enable MMU as default
    Atomic ops (e.g. LDREX) needs to enable MMU as default.
    And we removed unnecessary MMU mapping to save MMU TBL space.
    
    Signed-off-by: Savent Gate <savent_gate@outlook.com>

--- soc/fmsh/fmql/fmql/soc.c
+++ soc/fmsh/fmql/fmql/soc.c
@@ -9,6 +9,7 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/sys/sys_io.h>
 #include <zephyr/sys/util.h>
+#include <zephyr/arch/cache.h>
 
 #include <cmsis_core.h>
 #include <zephyr/arch/arm/mmu/arm_mmu.h>
@@ -19,20 +20,29 @@
 #define SLCR_UNLOCK_KEY 0xdf0d
 
 #define M_DEVICE_UNALIGNED (MT_NORMAL | MPERM_R | MPERM_W | MATTR_SHARED)
-#define M_DEVICE           (MT_DEVICE | MATTR_SHARED | MPERM_R | MPERM_W)
+#define M_DEVICE           (MT_DEVICE | MPERM_R | MPERM_W | MATTR_SHARED)
+#define M_MEMORY           (MT_NORMAL | MPERM_R | MPERM_W | MPERM_X | MATTR_SHARED)
+#define M_MEMORY_UNCACHED  (MT_STRONGLY_ORDERED | MPERM_R | MPERM_X | MATTR_SHARED)
 
 static const struct arm_mmu_region mmu_regions[] = {
 
-	MMU_REGION_FLAT_ENTRY("sram0", DT_REG_ADDR(DT_NODELABEL(sram0)),
-			      DT_REG_SIZE(DT_NODELABEL(sram0)),
-			      MT_STRONGLY_ORDERED | MPERM_R | MPERM_W | MPERM_X),
-	MMU_REGION_FLAT_ENTRY("ocm_low", DT_REG_ADDR(DT_NODELABEL(ocm_low)),
-			      DT_REG_SIZE(DT_NODELABEL(ocm_low)),
-			      MT_STRONGLY_ORDERED | MPERM_R | MPERM_W | MPERM_X),
+	/* Relocate vector address to 0x00000000, possible sram is ddr or ocm */
+	MMU_REGION_ENTRY("vector", DT_REG_ADDR(DT_CHOSEN(zephyr_sram)), 0x00000000,
+			 CONFIG_MMU_PAGE_SIZE, M_MEMORY_UNCACHED),
 
-	MMU_REGION_FLAT_ENTRY("ocm_high", DT_REG_ADDR(DT_NODELABEL(ocm_high)),
-			      DT_REG_SIZE(DT_NODELABEL(ocm_high)),
-			      MT_STRONGLY_ORDERED | MPERM_R | MPERM_W | MPERM_X),
+	/**
+	 * @note Zephyr already has mapped the generic regions (e.g. .text, .data, .bss)
+	 *
+		MMU_REGION_FLAT_ENTRY("ocm_low",
+					  DT_REG_ADDR(DT_NODELABEL(ocm_low)),
+					  DT_REG_SIZE(DT_NODELABEL(ocm_low)),
+					  M_MEMORY),
+
+		MMU_REGION_FLAT_ENTRY("ocm_high",
+					  DT_REG_ADDR(DT_NODELABEL(ocm_high)),
+					  DT_REG_SIZE(DT_NODELABEL(ocm_high)),
+					  M_MEMORY),
+	*/
 
 	MMU_REGION_FLAT_ENTRY("gic_dist", DT_REG_ADDR_BY_IDX(DT_NODELABEL(gic), 0),
 			      DT_REG_SIZE_BY_IDX(DT_NODELABEL(gic), 0), M_DEVICE),
@@ -40,45 +50,11 @@ static const struct arm_mmu_region mmu_regions[] = {
 	MMU_REGION_FLAT_ENTRY("gic_cpu", DT_REG_ADDR_BY_IDX(DT_NODELABEL(gic), 1),
 			      DT_REG_SIZE_BY_IDX(DT_NODELABEL(gic), 1), M_DEVICE),
 
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(uart0), okay)
-	MMU_REGION_FLAT_ENTRY("uart0", DT_REG_ADDR(DT_NODELABEL(uart0)),
-			      DT_REG_SIZE(DT_NODELABEL(uart0)), M_DEVICE),
-#endif
-
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(uart1), okay)
-	MMU_REGION_FLAT_ENTRY("uart1", DT_REG_ADDR(DT_NODELABEL(uart1)),
-			      DT_REG_SIZE(DT_NODELABEL(uart1)), M_DEVICE),
-#endif
-
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(gpio0), okay)
-	MMU_REGION_FLAT_ENTRY("gpio0", DT_REG_ADDR(DT_NODELABEL(gpio0)),
-			      DT_REG_SIZE(DT_NODELABEL(gpio0)), M_DEVICE),
-#endif
-
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(gpio1), okay)
-	MMU_REGION_FLAT_ENTRY("gpio1", DT_REG_ADDR(DT_NODELABEL(gpio1)),
-			      DT_REG_SIZE(DT_NODELABEL(gpio1)), M_DEVICE),
-#endif
-
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(gpio2), okay)
-	MMU_REGION_FLAT_ENTRY("gpio2", DT_REG_ADDR(DT_NODELABEL(gpio2)),
-			      DT_REG_SIZE(DT_NODELABEL(gpio2)), M_DEVICE),
-#endif
-
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(gpio3), okay)
-	MMU_REGION_FLAT_ENTRY("gpio3", DT_REG_ADDR(DT_NODELABEL(gpio3)),
-			      DT_REG_SIZE(DT_NODELABEL(gpio3)), M_DEVICE),
-#endif
-
 #if DT_NODE_HAS_STATUS(DT_NODELABEL(arch_timer), okay)
 	MMU_REGION_FLAT_ENTRY("arch_timer", DT_REG_ADDR(DT_NODELABEL(arch_timer)),
 			      DT_REG_SIZE(DT_NODELABEL(arch_timer)), M_DEVICE),
 #endif
 
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(slcr), okay)
-	MMU_REGION_FLAT_ENTRY("slcr", DT_REG_ADDR(DT_NODELABEL(slcr)),
-			      DT_REG_SIZE(DT_NODELABEL(slcr)), M_DEVICE),
-#endif
 };
 
 const struct arm_mmu_config mmu_config = {
@@ -120,9 +96,23 @@ void soc_reset_hook(void)
 
 	sctlr &= ~SCTLR_I_Msk;
 	sctlr &= ~SCTLR_C_Msk;
-	/* sctlr &= ~SCTLR_A_Msk; */
+	sctlr &= ~SCTLR_A_Msk;
 	sctlr &= ~SCTLR_M_Msk;
 	__set_SCTLR(sctlr);
+
+	/* Enable SMP */
+	__set_ACTLR(__get_ACTLR() | ACTLR_SMP_Msk);
+
+	/* invalidate dcache all, invalidate mmu tlb */
+	arch_dcache_invd_all();
+
+	/* Set TTBCR to disable LPAE */
+	__set_TTBR0(0);
+
+	/* Invalidate TLB */
+	__set_TLBIALL(0);
+	barrier_dsync_fence_full();
+	barrier_isync_fence_full();
 
 #if DT_NODE_HAS_STATUS(DT_NODELABEL(slcr), okay)
 	mm_reg_t addr = DT_REG_ADDR(DT_NODELABEL(slcr));
@@ -130,8 +120,17 @@ void soc_reset_hook(void)
 	/* Unlock System Level Control Registers (SLCR) */
 	sys_write32(SLCR_UNLOCK_KEY, addr + SLCR_UNLOCK);
 #endif
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(scu), okay)
+	mm_reg_t scu_base = DT_REG_ADDR(DT_NODELABEL(scu));
+
+	/* Enable the SCU */
+	scu_enable(scu_base);
+#endif
 }
 
+/* NOTE: If the vector table is not at address zero, relocate it */
+#ifndef CONFIG_MMU
 extern void *_vector_table[];
 #define VECTOR_ADDRESS ((uintptr_t)_vector_table)
 void relocate_vector_table(void)
@@ -140,3 +139,4 @@ void relocate_vector_table(void)
 	write_vbar(VECTOR_ADDRESS & VBAR_MASK);
 	barrier_isync_fence_full();
 }
+#endif
