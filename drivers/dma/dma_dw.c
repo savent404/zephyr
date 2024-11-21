26b01c828ae1f79ba6d9d74747e159e2a6acae66
Savent Gate <savent_gate@outlook.com>
2024-11-21 20:16:46 +0800

    Revert "board: rbs2530_cpu: dma: adapt dma and add dma sample"
    This reverts commit a1cd9f430c3387238f90803ac305019b930561cc.

--- drivers/dma/dma_dw.c
+++ drivers/dma/dma_dw.c
@@ -108,7 +108,7 @@ static const struct dma_driver_api dw_dma_driver_api = {
 			    &dw_dma_init,				\
 			    NULL,					\
 			    &dw_dma##inst##_data,			\
-			    &dw_dma##inst##_config, PRE_KERNEL_1,	\
+			    &dw_dma##inst##_config, POST_KERNEL,	\
 			    CONFIG_DMA_INIT_PRIORITY,			\
 			    &dw_dma_driver_api);			\
 									\
@@ -117,7 +117,7 @@ static const struct dma_driver_api dw_dma_driver_api = {
 		IRQ_CONNECT(DT_INST_IRQN(inst),				\
 			    DT_INST_IRQ(inst, priority), dw_dma_isr,	\
 			    DEVICE_DT_INST_GET(inst),			\
-			    DT_INST_IRQ(inst, flags));			\
+			    DT_INST_IRQ(inst, sense));			\
 		irq_enable(DT_INST_IRQN(inst));				\
 	}
 
