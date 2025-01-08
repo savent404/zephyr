8406e200b9224672f5ca43fd46488dcca80916e0
Savent Gate <savent_gate@outlook.com>
2025-01-08 12:52:06 +0800

    mcb: basic implement
    According to the feature specification.
    
    Signed-off-by: Savent Gate <savent_gate@outlook.com>

--- drivers/mcb/mcb_systech.c
+++ drivers/mcb/mcb_systech.c
@@ -9,7 +9,11 @@
  * @license This project is CLOSED SOURCE, All Rights Reserved
  *
  */
+#include <zephyr/devicetree.h>
+#include <zephyr/device.h>
+#include <zephyr/types.h>
 #include <zephyr/drivers/mcb.h>
+#include "mcb_systech.h"
 
 #define LOG_LEVEL CONFIG_MCB_LOG_LEVEL
 #include <zephyr/logging/log.h>
@@ -17,157 +21,351 @@ LOG_MODULE_REGISTER(mcb_systech);
 
 #define DT_DRV_COMPAT systech_mcb
 
+#define DEV_CFG(_dev)  ((const struct mcb_systech_config *const)(_dev)->config)
+#define DEV_DATA(_dev) ((struct mcb_systech_data *const)(_dev)->data)
+
 struct mcb_systech_data {
+	DEVICE_MMIO_NAMED_RAM(reg);
+	DEVICE_MMIO_NAMED_RAM(rx_buf);
+	DEVICE_MMIO_NAMED_RAM(tx_buf);
+	uint32_t bandwidth;
+	uint32_t pool_time;
 };
 
 struct mcb_systech_config {
-	/* Region 0 */
-	uintptr_t reg_base;
-	size_t reg_size;
-	/* Region 1 */
-	uintptr_t tx_base;
-	size_t tx_size;
-	/* Region 2 */
-	uintptr_t rx_base;
-	size_t rx_size;
+	DEVICE_MMIO_NAMED_ROM(reg);
+	DEVICE_MMIO_NAMED_ROM(rx_buf);
+	DEVICE_MMIO_NAMED_ROM(tx_buf);
+	uint32_t max_bandwidth;
 };
 
 void mcb_systech_reset(const struct device *dev, uint8_t role)
 {
-	const struct mcb_systech_config *config = dev->config;
-	struct mcb_systech_data *data = dev->data;
+	uint32_t reg_base = DEVICE_MMIO_NAMED_GET(dev, reg);
+	uint32_t val;
 
-	ARG_UNUSED(data);
-	ARG_UNUSED(config);
+	/* Clear CTRL/STATUS register */
+	sys_write32(0, reg_base + MCB_REG_CTRL1);
+	sys_write32(0, reg_base + MCB_REG_CTRL2);
+	sys_write32(0, reg_base + MCB_REG_STATUS1);
+	sys_write32(0, reg_base + MCB_REG_I_A_COUNT);
+	sys_write32(0, reg_base + MCB_REG_I_B_COUNT);
+
+	for (uint32_t addr = MCB_REG_PORT_MASK0; addr <= MCB_REG_PORT_MASK3; addr += 4) {
+		sys_write32(0, reg_base + addr);
+	}
+
+	for (uint32_t addr = MCB_REG_PORT_RDY_MASK0; addr <= MCB_REG_PORT_RDY_MASK3; addr += 4) {
+		sys_write32(0, reg_base + addr);
+	}
+
+	for (uint32_t addr = MCB_REG_PORT_W_MASK0; addr <= MCB_REG_PORT_W_MASK3; addr += 4) {
+		sys_write32(0, reg_base + addr);
+	}
+
+	for (uint32_t addr = MCB_REG_PORT_RX_LEN(0); addr <= MCB_REG_PORT_RX_LEN(0x80); addr += 4) {
+		sys_write32(0, reg_base + addr);
+	}
+
+	for (uint32_t addr = MCB_REG_PORT_RX_MAX(0); addr <= MCB_REG_PORT_RX_MAX(0x80); addr += 4) {
+		sys_write32(0, reg_base + addr);
+	}
+
+	for (uint32_t addr = MCB_REG_PORT_TX_LEN(0); addr <= MCB_REG_PORT_TX_LEN(0x80); addr += 4) {
+		sys_write32(0, reg_base + addr);
+	}
+
+	for (uint32_t addr = MCB_REG_PORT_RX_SID(0); addr <= MCB_REG_PORT_RX_SID(0x80); addr += 4) {
+		sys_write32(0, reg_base + addr);
+	}
+
+	/* FIXME: Set DT and DR via DeviceTree or user configuration */
+	switch (role) {
+	case _MCB_ROLE_MASTER:
+		val = VALUE2REG(CTRL2, DR, DR_MPU_P) | VALUE2REG(CTRL2, DT, DT_MPU_P);
+		LOG_DBG("Set MCB to master mode");
+		break;
+	case _MCB_ROLE_SLAVE:
+	default:
+		val = VALUE2REG(CTRL2, DR, DR_MPU_B) | VALUE2REG(CTRL2, DT, DT_MPU_B);
+		LOG_DBG("Set MCB to slave mode");
+		break;
+	}
+	sys_write32(val, reg_base + MCB_REG_CTRL2);
+
+	/* Enable RxEN as default */
+	sys_write32(b_MCB_CTRL1_RxEN, reg_base + MCB_REG_CTRL1);
 }
 
 void mcb_systech_poll_time(const struct device *dev, uint32_t timeout)
 {
-	const struct mcb_systech_config *config = dev->config;
+	uint32_t reg_base = DEVICE_MMIO_NAMED_GET(dev, reg);
 	struct mcb_systech_data *data = dev->data;
+	uint32_t val;
 
-	ARG_UNUSED(data);
-	ARG_UNUSED(config);
+	timeout = timeout / 10; /* Register POLL_TIME is in 10ns unit */
+	data->pool_time = timeout;
+	val = sys_read32(reg_base + MCB_REG_CTRL1);
+	val &= ~_REG_MASK(CTRL2, PT);
+	val |= VALUE2REG(CTRL2, PT, timeout);
+	sys_write32(val, reg_base + MCB_REG_CTRL1);
+
+	LOG_DBG("Set poll time to %dns", timeout * 10);
+}
+
+static inline void _config_port_general(const struct device *dev, uint8_t port, bool enable,
+					bool write, uint32_t max_rx_len)
+{
+	uint32_t reg_base = DEVICE_MMIO_NAMED_GET(dev, reg);
+	uint32_t val;
+	uint32_t port_idx = port / 32;
+	uint32_t port_bit = BIT(port % 32);
+	/* setup none-critical parts */
+	sys_write32(max_rx_len, reg_base + MCB_REG_PORT_RX_MAX(port));
+	sys_write32(0, reg_base + MCB_REG_PORT_RX_SID(port));
+	sys_write32(0, reg_base + MCB_REG_PORT_RX_LEN(port));
+	sys_write32(0, reg_base + MCB_REG_PORT_TX_LEN(port));
+
+	/* clear port ready mask */
+	val = sys_read32(reg_base + MCB_REG_PORT_RDY_MASK0 + port_idx * 4);
+	val &= ~port_bit;
+	sys_write32(val, reg_base + MCB_REG_PORT_RDY_MASK0 + port_idx * 4);
+
+	/* reset write mask */
+	val = sys_read32(reg_base + MCB_REG_PORT_W_MASK0 + port_idx * 4);
+	val &= ~port_bit;
+	val |= write ? port_bit : 0;
+	sys_write32(val, reg_base + MCB_REG_PORT_W_MASK0 + port_idx * 4);
+
+	/* at least wipe potemtial ldp_a_header */
+	memset(mcb_get_tx_buf(dev, port), 0, 4);
+	memset(mcb_get_rx_buf(dev, port), 0, 4);
+	/* FIXME: Once buffer cached, need to flush cache */
 }
 
 void mcb_systech_config_port(const struct device *dev, uint8_t port, bool enable, bool write,
 			     uint16_t max_rx_len)
 {
-	const struct mcb_systech_config *config = dev->config;
-	struct mcb_systech_data *data = dev->data;
+	uint32_t reg_base = DEVICE_MMIO_NAMED_GET(dev, reg);
+	uint32_t val;
+	uint32_t port_idx = port / 32;
+	uint32_t port_bit = BIT(port % 32);
 
-	ARG_UNUSED(data);
-	ARG_UNUSED(config);
+	if (port >= MCB_MAX_PORT) {
+		LOG_ERR("Invalid port number");
+		return;
+	}
+
+	if (enable) {
+		_config_port_general(dev, port, enable, write, max_rx_len);
+
+		/* enable port */
+		val = sys_read32(reg_base + MCB_REG_PORT_MASK0 + port_idx * 4);
+		val |= port_bit;
+		sys_write32(val, reg_base + MCB_REG_PORT_MASK0 + port_idx * 4);
+	} else {
+		/* disable port */
+		val = sys_read32(reg_base + MCB_REG_PORT_MASK0 + port_idx * 4);
+		val &= ~port_bit;
+		sys_write32(val, reg_base + MCB_REG_PORT_MASK0 + port_idx * 4);
+
+		_config_port_general(dev, port, enable, write, max_rx_len);
+	}
 }
 
-void mcb_systech_get_status(const struct device *dev, uint32_t *status)
+void mcb_systech_tx(const struct device *dev, uint8_t sid, uint8_t port)
 {
-	const struct mcb_systech_config *config = dev->config;
-	struct mcb_systech_data *data = dev->data;
+	uint32_t reg_base = DEVICE_MMIO_NAMED_GET(dev, reg);
+	uint32_t val;
 
-	ARG_UNUSED(data);
-	ARG_UNUSED(config);
+	if (port >= MCB_MAX_PORT) {
+		LOG_ERR("Invalid port number");
+		return;
+	}
 
-	*status = _MCB_ERR_T_ERR | _MCB_ERR_R_ERR;
+	val = sys_read32(reg_base + MCB_REG_CTRL1);
+	val |= b_MCB_CTRL1_TxEN;
+	val &= _REG_MASK(CTRL1, D_SID);
+	val |= VALUE2REG(CTRL1, D_SID, sid);
+	val &= _REG_MASK(CTRL1, PORT);
+	val |= VALUE2REG(CTRL1, PORT, port);
+	sys_write32(val, reg_base + MCB_REG_CTRL1);
+}
+
+void mcb_systech_get_status(const struct device *dev, uint32_t *s)
+{
+	uint32_t reg_base = DEVICE_MMIO_NAMED_GET(dev, reg);
+	uint32_t val, status = 0;
+
+	val = sys_read32(reg_base + MCB_REG_STATUS1);
+
+	/* Do the mapping work */
+	if (val & b_MCB_STATUS1_RE) {
+		status |= _MCB_ERR_R_ERR;
+	}
+	if (val & b_MCB_STATUS1_TE) {
+		status |= _MCB_ERR_T_ERR;
+	}
+	if (val & b_MCB_STATUS1_IE) {
+		status |= _MCB_ERR_I_ERR;
+	}
+	if (val & b_MCB_STATUS1_RF) {
+		status |= _MCB_ERR_PREEMPT;
+	}
+	if (val & b_MCB_STATUS1_PE) {
+		status |= _MCB_ERR_P_ERR;
+	}
+
+	if (s) {
+		*s = status;
+	}
 }
 
 void mcb_systech_clr_status(const struct device *dev, uint32_t bits)
 {
-	const struct mcb_systech_config *config = dev->config;
-	struct mcb_systech_data *data = dev->data;
+	uint32_t reg_base = DEVICE_MMIO_NAMED_GET(dev, reg);
+	uint32_t val;
 
-	ARG_UNUSED(data);
-	ARG_UNUSED(config);
+	val = sys_read32(reg_base + MCB_REG_STATUS1);
+
+	if (bits & _MCB_ERR_R_ERR) {
+		val &= ~b_MCB_STATUS1_RE;
+	}
+	if (bits & _MCB_ERR_T_ERR) {
+		val &= ~b_MCB_STATUS1_TE;
+	}
+	if (bits & _MCB_ERR_I_ERR) {
+		val &= ~b_MCB_STATUS1_IE;
+	}
+	if (bits & _MCB_ERR_PREEMPT) {
+		val &= ~b_MCB_STATUS1_RF;
+	}
+	if (bits & _MCB_ERR_P_ERR) {
+		val &= ~b_MCB_STATUS1_PE;
+	}
+
+	sys_write32(val, reg_base + MCB_REG_STATUS1);
 }
 
 void *mcb_systech_get_rx_buf(const struct device *dev, uint8_t port)
 {
-	const struct mcb_systech_config *config = dev->config;
-	struct mcb_systech_data *data = dev->data;
+	uint32_t rx_buf_base = DEVICE_MMIO_NAMED_GET(dev, rx_buf);
 
-	ARG_UNUSED(data);
-	ARG_UNUSED(config);
+	if (port >= MCB_MAX_PORT) {
+		LOG_ERR("Invalid port number");
+		return NULL;
+	}
 
-	return NULL;
+	return (void *)(rx_buf_base + MCB_REG_PORT_RX(port));
 }
 
 void *mcb_systech_get_tx_buf(const struct device *dev, uint8_t port)
 {
-	const struct mcb_systech_config *config = dev->config;
-	struct mcb_systech_data *data = dev->data;
+	uint32_t tx_buf_base = DEVICE_MMIO_NAMED_GET(dev, tx_buf);
 
-	ARG_UNUSED(data);
-	ARG_UNUSED(config);
+	if (port >= MCB_MAX_PORT) {
+		LOG_ERR("Invalid port number");
+		return NULL;
+	}
 
-	return NULL;
+	return (void *)(tx_buf_base + MCB_REG_PORT_TX(port));
 }
 
 uint16_t mcb_systech_get_rx_len(const struct device *dev, uint8_t port)
 {
-	const struct mcb_systech_config *config = dev->config;
-	struct mcb_systech_data *data = dev->data;
+	uint32_t reg_base = DEVICE_MMIO_NAMED_GET(dev, reg);
+	uint32_t val;
 
-	ARG_UNUSED(data);
-	ARG_UNUSED(config);
-
-	return 0;
+	if (port >= MCB_MAX_PORT) {
+		LOG_ERR("Invalid port number");
+		return 0;
+	}
+	val = sys_read32(reg_base + MCB_REG_PORT_RX_LEN(port));
+	return val;
 }
 
 uint16_t mcb_systech_get_tx_len(const struct device *dev, uint8_t port)
 {
-	const struct mcb_systech_config *config = dev->config;
-	struct mcb_systech_data *data = dev->data;
+	uint32_t reg_base = DEVICE_MMIO_NAMED_GET(dev, reg);
+	uint32_t val;
 
-	ARG_UNUSED(data);
-	ARG_UNUSED(config);
-
-	return 0;
+	if (port >= MCB_MAX_PORT) {
+		LOG_ERR("Invalid port number");
+		return 0;
+	}
+	val = sys_read32(reg_base + MCB_REG_PORT_TX_LEN(port));
+	return val;
 }
 
 void mcb_systech_set_tx_len(const struct device *dev, uint8_t port, uint16_t len)
 {
-	const struct mcb_systech_config *config = dev->config;
-	struct mcb_systech_data *data = dev->data;
+	uint32_t reg_base = DEVICE_MMIO_NAMED_GET(dev, reg);
 
-	ARG_UNUSED(data);
-	ARG_UNUSED(config);
+	if (port >= MCB_MAX_PORT) {
+		LOG_ERR("Invalid port number");
+		return;
+	}
+	sys_write32(len, reg_base + MCB_REG_PORT_TX_LEN(port));
 }
 
 void mcb_systech_rx_clr(const struct device *dev, uint8_t port)
 {
-	const struct mcb_systech_config *config = dev->config;
-	struct mcb_systech_data *data = dev->data;
+	uint32_t reg_base = DEVICE_MMIO_NAMED_GET(dev, reg);
+	uint32_t val;
 
-	ARG_UNUSED(data);
-	ARG_UNUSED(config);
+	val = sys_read32(reg_base + MCB_REG_STATUS1);
+	val &= ~b_MCB_STATUS1_RDY;
+	sys_write32(val, reg_base + MCB_REG_STATUS1);
+
+	if (port >= MCB_MAX_PORT) {
+		LOG_ERR("Invalid port number");
+		return;
+	}
+	val = sys_read32(reg_base + MCB_REG_PORT_RDY_MASK0 + (port / 32) * 4);
+	val &= ~BIT(port % 32);
+	sys_write32(val, reg_base + MCB_REG_PORT_RDY_MASK0 + (port / 32) * 4);
 }
 
 int mcb_systech_rx_is_ready(const struct device *dev, uint8_t port)
 {
-	const struct mcb_systech_config *config = dev->config;
-	struct mcb_systech_data *data = dev->data;
+	uint32_t reg_base = DEVICE_MMIO_NAMED_GET(dev, reg);
+	uint32_t val, reg;
 
-	ARG_UNUSED(data);
-	ARG_UNUSED(config);
+	if (port >= MCB_MAX_PORT) {
+		LOG_ERR("Invalid port number");
+		return -EINVAL;
+	}
 
+	reg = MCB_REG_PORT_RDY_MASK0 + (port / 32) * 4;
+	val = sys_read32(reg_base + reg);
+	if (val & BIT(port % 32)) {
+		return 1;
+	}
 	return 0;
 }
 
 static const struct mcb_driver_api mcb_systech_api = {
 	.reset = mcb_systech_reset,
+	.poll_time = mcb_systech_poll_time,
+	.config_port = mcb_systech_config_port,
+	.get_status = mcb_systech_get_status,
+	.clr_status = mcb_systech_clr_status,
+	.get_rx_buf = mcb_systech_get_rx_buf,
+	.get_tx_buf = mcb_systech_get_tx_buf,
+	.get_rx_len = mcb_systech_get_rx_len,
+	.get_tx_len = mcb_systech_get_tx_len,
+	.set_tx_len = mcb_systech_set_tx_len,
+	.rx_clr = mcb_systech_rx_clr,
+	.rx_is_ready = mcb_systech_rx_is_ready,
+	.tx = mcb_systech_tx,
 };
 
 static int mcb_systech_init(const struct device *dev)
 {
-	const struct mcb_systech_config *config = dev->config;
-	struct mcb_systech_data *data = dev->data;
-
-	ARG_UNUSED(data);
-	ARG_UNUSED(config);
-
-	/* FIXME: Do the MMIO mapping here */
-
-	LOG_DBG("MCB init");
+	DEVICE_MMIO_NAMED_MAP(dev, reg, K_MEM_CACHE_NONE);
+	/* FIXME: For tx_buf and rx_buf, enable cache would be better idea */
+	DEVICE_MMIO_NAMED_MAP(dev, rx_buf, K_MEM_CACHE_NONE);
+	DEVICE_MMIO_NAMED_MAP(dev, tx_buf, K_MEM_CACHE_NONE);
 
 	return 0;
 }
@@ -175,12 +373,10 @@ static int mcb_systech_init(const struct device *dev)
 #define MCB_SYSTECH_DEVICE(n)                                                                      \
 	static struct mcb_systech_data mcb_systech_data_##n;                                       \
 	static const struct mcb_systech_config mcb_systech_config_##n = {                          \
-		.reg_base = DT_INST_REG_ADDR_BY_IDX(n, 0),                                         \
-		.reg_size = DT_INST_REG_SIZE_BY_IDX(n, 0),                                         \
-		.tx_base = DT_INST_REG_ADDR_BY_IDX(n, 1),                                          \
-		.tx_size = DT_INST_REG_SIZE_BY_IDX(n, 1),                                          \
-		.rx_base = DT_INST_REG_ADDR_BY_IDX(n, 2),                                          \
-		.rx_size = DT_INST_REG_SIZE_BY_IDX(n, 2),                                          \
+		DEVICE_MMIO_NAMED_ROM_INIT(reg, DT_DRV_INST(n)),                                   \
+		DEVICE_MMIO_NAMED_ROM_INIT(rx_buf, DT_DRV_INST(n)),                                \
+		DEVICE_MMIO_NAMED_ROM_INIT(tx_buf, DT_DRV_INST(n)),                                \
+		.max_bandwidth = DT_INST_PROP(n, bandwidth),                                       \
 	};                                                                                         \
 	DEVICE_DT_INST_DEFINE(n, &mcb_systech_init, NULL, &mcb_systech_data_##n,                   \
 			      &mcb_systech_config_##n, POST_KERNEL, CONFIG_MCB_INIT_PRIORITY,      \
---
+++ drivers/mcb/mcb_systech.h
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2025 SYSTech Co.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#pragma once
+
+#include <zephyr/kernel.h>
+
+#define MCB_MAX_PORT 0x80
+
+#define MCB_REG_CTRL1          0x0000
+#define MCB_REG_CTRL2          0x0004
+#define MCB_REG_STATUS1        0x0008
+#define MCB_REG_I_A_COUNT      0x000C
+#define MCB_REG_I_B_COUNT      0x0010
+#define MCB_REG_DEVICE_INF0    0x0014
+#define MCB_REG_DEVICE_INF1    0x0018
+#define MCB_REG_DEVICE_INF2    0x001C
+#define MCB_REG_PORT_MASK0     0x0020
+#define MCB_REG_PORT_MASK1     0x0024
+#define MCB_REG_PORT_MASK2     0x0028
+#define MCB_REG_PORT_MASK3     0x002C
+#define MCB_REG_PORT_RDY_MASK0 0x0040
+#define MCB_REG_PORT_RDY_MASK1 0x0044
+#define MCB_REG_PORT_RDY_MASK2 0x0048
+#define MCB_REG_PORT_RDY_MASK3 0x004C
+#define MCB_REG_PORT_W_MASK0   0x0060
+#define MCB_REG_PORT_W_MASK1   0x0064
+#define MCB_REG_PORT_W_MASK2   0x0068
+#define MCB_REG_PORT_W_MASK3   0x006C
+#define MCB_REG_PORT_RX_LEN(n) (0x0100 + (n * 4))
+#define MCB_REG_PORT_RX_MAX(n) (0x0200 + (n * 4))
+#define MCB_REG_PORT_TX_LEN(n) (0x0300 + (n * 4))
+#define MCB_REG_PORT_RX_SID(n) (0x0400 + (n * 4))
+#define MCB_REG_PORT_RX(n)     (n * 0x800)
+#define MCB_REG_PORT_TX(n)     (n * 0x800)
+
+/* For MCB_CTRL1 */
+#define b_MCB_CTRL1_TxEN       BIT(0)
+#define b_MCB_CTRL1_RxEN       BIT(1)
+#define r_MCB_CTRL1_R_Ack_pos  2
+#define r_MCB_CTRL1_R_Ack_mask (BIT(2) - 1)
+#define R_Ack_echo             0
+#define R_Ack_reset            1
+#define R_Ack_set              2
+#define r_MCB_CTRL1_PORT_pos   8
+#define r_MCB_CTRL1_PORT_mask  (BIT(8) - 1)
+#define r_MCB_CTRL1_D_SID_pos  16
+#define r_MCB_CTRL1_D_SID_mask (BIT(8) - 1)
+#define r_MCB_CTRL1_S_SID_pos  24
+#define r_MCB_CTRL1_S_SID_mask (BIT(8) - 1)
+
+/* For MCB_CTRL2 */
+#define r_MCB_CTRL2_DR_pos  0
+#define r_MCB_CTRL2_DR_mask (BIT(8) - 1)
+#define DR_Unknown          0x00
+#define DR_MPU_B            0x01
+#define DR_EXT_B            0x02
+#define DR_IO               0x03
+#define DR_ETH              0x04
+#define DR_MPU_P            0x80
+#define DR_EXT_P            0x81
+#define r_MCB_CTRL2_DT_pos  8
+#define r_MCB_CTRL2_DT_mask (BIT(8) - 1)
+#define DT_Unknown          0x00
+#define DT_MPU_P            0x01
+#define DT_MPU_B            0x02
+#define DT_IO_AI            0x10
+#define DT_IO_AO            0x11
+#define DT_IO_DI            0x12
+#define DT_IO_DO            0x13
+#define DT_ETH              0x20
+#define DT_CAN              0x21
+#define DT_EXT_P            0x30
+#define DT_EXT_B            0x31
+#define r_MCB_CTRL2_PT_pos  16
+#define r_MCB_CTRL2_PT_mask (BIT(16) - 1)
+
+/* For MCB_STATUS1 */
+#define b_MCB_STATUS1_RE          BIT(0)
+#define b_MCB_STATUS1_TE          BIT(1)
+#define b_MCB_STATUS1_IE          BIT(2)
+#define b_MCB_STATUS1_RF          BIT(3)
+#define b_MCB_STATUS1_PE          BIT(4)
+#define b_MCB_STATUS1_RDY         BIT(7)
+#define r_MCB_STATUS1_RxPort_pos  8
+#define r_MCB_STATUS1_RxPort_mask (BIT(8) - 1)
+
+#define _REG_MASK(reg, bit_name) r_MCB_##reg##_##bit_name##_mask
+#define _REG_POS(reg, bit_name)  r_MCB_##reg##_##bit_name##_pos
+#define REG2VALUE(reg, bit_name, value)                                                            \
+	(((value) >> _REG_POS(reg, bit_name)) & _REG_MASK(reg, bit_name))
+#define VALUE2REG(reg, bit_name, value)                                                            \
+	(((value) & _REG_MASK(reg, bit_name)) << _REG_POS(reg, bit_name))
