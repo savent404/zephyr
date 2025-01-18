fa3cd89b6f9322e321ea955c92bfca7802625d6e
Hei Wei Lu <heiweilu@gamil.com>
2025-01-18 23:38:30 +0800

    rbs2530_cpu: iic_rtc: drivers with nxp,pcf8563
    Modify the Copyright、revise rtc0 address and register configuration
    
    Signed-off-by: Hei Wei Lu <heiweilu@gamil.com>

---
+++ samples/drivers/rtc/src/main.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2025 SYSTech Co.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <string.h>
+
+#include <zephyr/kernel.h>
+
+int main(void)
+{
+	k_sleep(K_FOREVER);
+	return 0;
+}
