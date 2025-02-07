6bc13cca8be14d1f2385a3d1be09dc65554a6a61
Hei Wei Lu <heiweilu@gmail.com>
2025-02-07 10:44:50 +0800

    sample: button_irq: add sample to rbs2530_cpu
    Brief
    - gpio_dw.c and input_gpio_keys.c driver has adapted the interrupt.
    - Because the test hardware has no buttons, use the SD io pin (mio39).
    
    Signed-off-by: Hei Wei Lu <heiweilu@gmail.com>

---
+++ samples/basic/button_irq/src/main.c
@@ -0,0 +1,66 @@
+/*
+ * @copyright Copyright (c) 2025 SYSTech Co.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/sys/printk.h>
+#include <inttypes.h>
+
+/*
+ * Get button configuration from the devicetree sw0 alias. This is mandatory.
+ */
+#define SW0_NODE DT_ALIAS(sw0)
+#if !DT_NODE_HAS_STATUS(SW0_NODE, okay)
+#error "Unsupported board: sw0 devicetree alias is not defined"
+#endif
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios, {0});
+static struct gpio_callback button_cb_data;
+
+void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	ARG_UNUSED(dev);
+	ARG_UNUSED(cb);
+	ARG_UNUSED(pins);
+	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
+}
+
+int main(void)
+{
+	int ret;
+
+	if (!gpio_is_ready_dt(&button)) {
+		printk("Error: button device %s is not ready\n", button.port->name);
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (ret != 0) {
+		printk("Error %d: failed to configure %s pin %d\n", ret, button.port->name,
+		       button.pin);
+		return 0;
+	}
+
+	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret != 0) {
+		printk("Error %d: failed to configure interrupt on %s pin %d\n", ret,
+		       button.port->name, button.pin);
+		return 0;
+	}
+
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+	gpio_add_callback(button.port, &button_cb_data);
+	printk("Set up button at %s pin %d\n", button.port->name, button.pin);
+
+	printk("Press the button......\n");
+
+	while (1) {
+		k_msleep(100);
+	}
+
+	return 0;
+}
