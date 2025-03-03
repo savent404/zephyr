2757133583c97586e0498b09c3a7c293c3a08b8a
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2025-03-03 11:30:25 +0200

    net: if: Release the interface lock early in IPv6 RS timeout handler
    The net_if.c:rs_timeout() is sending a new IPv6 router solicitation
    message to network by calling net_if_start_rs(). That function will
    then acquire iface->lock and call net_ipv6_start_rs() which will try
    to send the RS message and acquire TX send lock.
    During this RS send, we might receive TCP data that could try to
    send an ack to peer. This will then in turn cause also TX lock
    to be acquired. Depending on timing, the lock ordering between
    rx thread and system workq might mix which could lead to deadlock.
    Fix this issue by releasing the iface->lock before starting the
    RS sending process. The net_if_start_rs() does not really need to
    keep the interface lock for a long time as it is the only one sending
    the RS message.
    
    Fixes #86499
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>
    Signed-off-by: Savent Gate <savent_gate@outlook.com>

--- subsys/net/ip/net_if.c
+++ subsys/net/ip/net_if.c
@@ -1509,6 +1509,8 @@ void net_if_start_rs(struct net_if *iface)
 		goto out;
 	}
 
+	net_if_unlock(iface);
+
 	NET_DBG("Starting ND/RS for iface %p", iface);
 
 	if (!net_ipv6_start_rs(iface)) {
@@ -1524,6 +1526,7 @@ void net_if_start_rs(struct net_if *iface)
 		}
 	}
 
+	return;
 out:
 	net_if_unlock(iface);
 }
