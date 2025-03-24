b6ab1e791b9d180fc2b3cd97c5f7a23a38c01f3a
Savet Gate <savent_gate@outlook.com>
2025-03-24 16:40:30 +0800

    cmake: git describe prefer tags
    The tags is not the perfered choice currently.
    
    Signed-off-by: Savet Gate <savent_gate@outlook.com>

--- cmake/modules/git.cmake
+++ cmake/modules/git.cmake
@@ -8,13 +8,13 @@ find_package(Git QUIET)
 #   git_describe(<dir> <output>)
 #
 # Helper function to get a short GIT desciption associated with a directory.
-# OUTPUT is set to the output of `git describe --abbrev=12 --always` as run
+# OUTPUT is set to the output of `git describe --abbrev=12 --always --tags` as run
 # from DIR.
 #
 function(git_describe DIR OUTPUT)
   if(GIT_FOUND)
     execute_process(
-      COMMAND ${GIT_EXECUTABLE} describe --abbrev=12 --always
+      COMMAND ${GIT_EXECUTABLE} describe --abbrev=12 --always --tags
       WORKING_DIRECTORY                ${DIR}
       OUTPUT_VARIABLE                  DESCRIPTION
       OUTPUT_STRIP_TRAILING_WHITESPACE
